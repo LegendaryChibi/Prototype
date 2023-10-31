67819b5d14b1dccdf307e82a848ee57966b564a8
LegendaryChibi <80108408+LegendaryChibi@users.noreply.github.com>
2023-10-30 23:04:53 -0400

    Initial Commit

---
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    private Vector3 direction;
+
+    private void Update()
+    {
+        //Move bullet
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    public void Fire(Vector3 dir)
+    {
+        //Direction to fire.
+        direction = dir;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Destory if wall is hit.
+        if (other.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
